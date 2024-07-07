5903dd99f6a582fa07496caf8676432c0b988503
Maciel Ripetti <maciel.ripetti@gmail.com>
2024-07-07 17:46:23 -0400

    modelos y admin

--- CER3/core/admin.py
+++ CER3/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Planta, Combustible, RegistroProduccion
 
-# Register your models here.
+# Registrar Modelos en el Admin de Django
+admin.site.register(Planta)
+admin.site.register(Combustible)
+
+# Configuración del modelo RegistroProduccion
+class RegistroProduccionAdmin(admin.ModelAdmin):
+    list_display = ('combustible', 'litros_producidos', 'fecha_produccion', 'turno', 'hora_registro', 'operador')
+    list_filter = ('fecha_produccion', 'turno', 'operador')
+    search_fields = ('combustible__nombre', 'operador__username')
+
+admin.site.register(RegistroProduccion, RegistroProduccionAdmin)
--- CER3/core/models.py
+++ CER3/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Planta(models.Model):
+    codigo = models.CharField(max_length=3, unique=True)
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Combustible(models.Model):
+    codigo = models.CharField(max_length=3, unique=True)
+    nombre = models.CharField(max_length=100)
+    planta = models.ForeignKey(Planta, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.codigo})"
+
+class RegistroProduccion(models.Model):
+    TURNO_CHOICES = [
+        ('AM', 'Mañana'),
+        ('PM', 'Tarde'),
+        ('MM', 'Noche'),
+    ]
+
+    combustible = models.ForeignKey(Combustible, on_delete=models.CASCADE)
+    litros_producidos = models.FloatField()
+    fecha_produccion = models.DateField()
+    turno = models.CharField(max_length=2, choices=TURNO_CHOICES)
+    hora_registro = models.TimeField(auto_now_add=True)
+    operador = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.combustible.nombre} - {self.fecha_produccion} - {self.turno}"
