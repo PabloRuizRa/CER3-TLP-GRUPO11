1808fbce1cdcac3da35ee4d1245d950b1ec2b6a1
Maciel Ripetti <maciel.ripetti@gmail.com>
2024-07-07 18:32:10 -0400

    urls de registro

--- CER3/CER3/urls.py
+++ CER3/CER3/urls.py
@@ -4,9 +4,8 @@ from core import views as registro_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', registro_views.registrar_produccion, name='home'),  # Esto es solo un ejemplo, ajusta según sea necesario
-    path('registro/registrar/', registro_views.registrar_produccion, name='registrar_produccion'),
-    path('registro/exito/', registro_views.registro_exitoso, name='registro_exitoso'),
+    path('', registro_views.home),  
+    path('registro/', registro_views.registrar_produccion),
     path('accounts/', include('django.contrib.auth.urls')),
     path('auth/', include('rest_framework.urls')),
 ]
