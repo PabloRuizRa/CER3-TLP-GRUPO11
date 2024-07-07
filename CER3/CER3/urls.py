c99e78bfa51606e4e61e2916f1849e5725c75e34
Maciel Ripetti <maciel.ripetti@gmail.com>
2024-07-07 17:45:40 -0400

    configurar vistas

--- CER3/CER3/urls.py
+++ CER3/CER3/urls.py
@@ -1,25 +1,12 @@
-"""
-URL configuration for CER3 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from core import views
+from core import views as registro_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('API.urls')),
-    path('', views.home, name="home"),
+    path('', registro_views.registrar_produccion, name='home'),  # Esto es solo un ejemplo, ajusta según sea necesario
+    path('registro/registrar/', registro_views.registrar_produccion, name='registrar_produccion'),
+    path('registro/exito/', registro_views.registro_exitoso, name='registro_exitoso'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('auth/', include('rest_framework.urls')),
 ]
--- CER3/core/views.py
+++ CER3/core/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from rest_framework import viewsets
+from .forms import RegistroProduccionForm
+from .models import RegistroProduccion
+from .serializers import RegistroProduccionSerializer
 
-# Create your views here.
+@login_required
+def registrar_produccion(request):
+    if request.method == 'POST':
+        form = RegistroProduccionForm(request.POST)
+        if form.is_valid():
+            registro = form.save(commit=False)
+            registro.operador = request.user
+            registro.save()
+            return redirect('registro_exitoso')
+    else:
+        form = RegistroProduccionForm()
+    return render(request, 'core/base.html', {'form': form})
 
-def home(request: HttpRequest):
-    title = "Inicio"
+def registro_exitoso(request):
+    return render(request, 'core/registro/registro_exitoso.html')
 
-    data = {
-        "title" : title,
-    }
-
-    return render(request, 'core/base.html',data)
+class RegistroProduccionViewSet(viewsets.ModelViewSet):
+    queryset = RegistroProduccion.objects.all()
+    serializer_class = RegistroProduccionSerializer
+    filterset_fields = ['fecha_produccion', 'turno', 'operador', 'combustible']
